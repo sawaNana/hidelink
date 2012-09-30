5bd5636d3bb8e7d97cb89709f11dac795d80f041
sawaNana <sawa.nanako@gmail.com>
2012-09-30 20:05:06 +0900

    Clean  some dust.

--- chrome/hideLinkListener.js
+++ chrome/hideLinkListener.js
@@ -1,35 +1,33 @@
 var HideLinkListener = function() {
    this.linkIsHidden = false;
-   this.targetKey   = 65; // 'a'
-   this.anchors;
+   this.targetKey    = 65; // 'a'
 };
 HideLinkListener.prototype = {
    handleEvent : function(event) {
       if (this.linkIsHidden && (event.type === 'keyup' || event.type === 'blur'))
          this.resetLink();
       else if (event.keyCode == this.targetKey && event.type === 'keydown' 
-      	&& !(this.linkIsHidden || event.altKey  || event.shiftKey || event.ctrlKey )
+      	&& !(this.linkIsHidden || event.altKey  || event.shiftKey || event.ctrlKey ) )
          this.hideLink();
    },
    hideLink : function() {
       this.linkIsHidden = true;
-      this.anchors = document.getElementsByTagName('a');
-      var i;
-      for (i = 0; i < this.anchors.length; i++) {
-         var href = this.anchors[i].href;
-	 if (typeof href === 'undefined')
-	      continue;
+      this.anchors = document.querySelectorAll('a[href]');
+      var i, href;
+      var length = this.anchors.length;
+      for (i = 0; i < length; i++) {
+         href = this.anchors[i].getAttribute('href');
          this.anchors[i].setAttribute('data-hidelink-href-cache', href);
          this.anchors[i].removeAttribute('href');
       }
    },
    resetLink : function() {
-      var i;
-      for (i = 0; i < this.anchors.length; i++) {
-         var href = this.anchors[i].getAttribute('data-hidelink-href-cache'); 
-	 if (typeof href === 'undefined')
-	      continue;
+      var i, href;
+      var length = this.anchors.length;
+      for (i = 0; i < length; i++) {
+         href = this.anchors[i].getAttribute('data-hidelink-href-cache'); 
          this.anchors[i].setAttribute('href', href);
+         this.anchors[i].removeAttribute('data-hidelink-href-cache');
       }
       this.linkIsHidden = false;
    }
